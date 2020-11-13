18ed1a14847894751079303dd31d4706c39d275b
ShinySilver <nicolas.louismarie@gmail.com>
2020-11-13 10:46:18 +0100

    Improved Resources handling

---
+++ ias-fakedetection/resources/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+PATH = os.path.dirname(__file__)
+DATASETS = os.path.join(PATH, 'datasets')
+SAMPLE_DATASET = os.path.join(DATASETS, 'sample_dataset.npz')
---
+++ ias-fakedetection/resources/datasets/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+RESOURCES_PATH = os.path.dirname(__file__)
+DATASETS_PATH = os.path.join(RESOURCES_PATH, 'datasets')
