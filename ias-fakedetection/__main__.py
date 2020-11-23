d93bdf3aa081073d30a73f626889bcc6d944914a
ShinySilver <nicolas.louismarie@gmail.com>
2020-11-23 15:18:02 +0100

    Working NN

--- ias-fakedetection/__main__.py
+++ ias-fakedetection/__main__.py
@@ -1,7 +1,63 @@
 import resources.datasets as data
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from keras import models
+from keras import layers
+
+
+def short_print(db, head):
+    print(f"###############\n#    {head}\n###############\n")
+    for key, value in db.items():
+        print(f'###############\n# "{key}" (shape {np.array(value).shape}):\n{value[:min(len(value),50)]}\n')
 
 db_raw = data.load_raw()
-print(db_raw)
-
 db = data.load_BoW()
-print(db)
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(len(db['dictionnary']),)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+
+x_val_array = db["text_bow"][:2000]
+y_val = db["class_id"][:2000]
+partial_x_train_array = db["text_bow"][2000:]
+partial_y_train_array = db["class_id"][2000:]
+
+history = model.fit(partial_x_train_array,
+                    partial_y_train_array,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val_array, y_val))
+
+#plot the results :
+history_dict = history.history
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss, 'b.', label='Training loss')
+plt.plot(epochs, val_loss, 'r-', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+
+plt.figure()
+plt.plot(epochs, acc, 'b.', label='Training accuracy')
+plt.plot(epochs, val_acc, 'r-', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+
+plt.legend()
+plt.show()
--- ias-fakedetection/resources/datasets/__init__.py
+++ ias-fakedetection/resources/datasets/__init__.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 DATASET_PATH = os.path.join(os.path.dirname(__file__), 'news_fake_or_real.csv')
@@ -10,9 +11,9 @@ _DB_BOW = None
 def load_raw():
     global _DB_RAW
     if _DB_RAW is None:
-        db = dict(pd.read_csv(DATASET_PATH))
-        db['label_id']=[int(label=='REAL') for label in db['label']]
-        db['labels']=['FAKE','REAL']
+        db = pd.read_csv(DATASET_PATH).to_dict(orient='list')
+        db['class_id']=np.array([int(label=='REAL') for label in db['label']])
+        db['labels']=np.array(['FAKE','REAL'])
         _DB_RAW = db
         return db
     else:
@@ -24,8 +25,9 @@ def load_BoW():
         db = load_raw()
         vectorizer = CountVectorizer()
         vectorizer.fit(db['text'])
-        db['dictionnary'] = vectorizer.vocabulary_
-        db['indexed_text'] = vectorizer.transform(db['text'])
+        db['dictionnary'] = np.array(list(sorted(vectorizer.vocabulary_.keys(),
+                                                 key=lambda k:vectorizer.vocabulary_[k])))
+        db['text_bow'] = vectorizer.transform(db['text']).toarray()
         _DB_BOW = db
         return db
     else:
